3f4d834f42557eb423d44e8f5d602fae3f6d8bb7
egoekalp <erditaner.goekalp@eqs.com>
2021-03-29 10:28:41 +0200

    removed unused files

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,8 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const typescript = require('rollup-plugin-typescript2');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const babel = require('@rollup/plugin-babel');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const html2 = require('rollup-plugin-html2');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const resolve = require('@rollup/plugin-node-resolve');
@@ -22,11 +20,6 @@ module.exports = {
         html({
             include: '**/*.html'
         }),
-        // babel.default({
-        //     extensions: ['.js', '.ts'],
-        //     babelHelpers: 'bundled',
-        //     exclude: 'node_modules/**'
-        // }),
         typescript({tsconfig: './tsconfig.json'}),
         html2({
             template: './demo/index.html'
