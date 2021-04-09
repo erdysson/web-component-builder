c86c28a76f54327d11c4413e917340fdf44f5853
egoekalp <erditaner.goekalp@eqs.com>
2021-04-09 16:09:22 +0200

    removed polyfills.ts

--- src/lib/platforms/polyfills.ts
+++
@@ -1,5 +0,0 @@
-// needed for IE11
-import 'core-js/stable';
-import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
-// needed for es5 compatibility
-import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
