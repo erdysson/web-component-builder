be31aa69495cb61ef83ba5cce5ba115925653a67
egoekalp <erditaner.goekalp@eqs.com>
2021-04-12 15:38:15 +0200

    added missing polyfills

--- demo/app/main.ts
+++ demo/app/main.ts
@@ -1,3 +1,6 @@
+import 'core-js/stable';
+import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
+
 import {bootstrap} from 'web-component-builder';
 
 import {MainModule} from './main.module';
