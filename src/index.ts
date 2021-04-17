71462ee63501c077d9a9d4194d7fd45cc92f06d2
egoekalp <erditaner.goekalp@eqs.com>
2021-04-17 23:02:56 +0200

    removed commented out code lines

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 export * from './core/bootstrap';
 export * from './core/decorators';
 export * from './core/interfaces';
-// export * from './core/metadata';
-// export * from './core/metadata-interfaces';
-// export * from './core/metadata-keys';
-// export * from './core/runtime';
