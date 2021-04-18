a1fb8a10ce038e896fbb97f29eb7f8a3f1abfcb4
egoekalp <erditaner.goekalp@eqs.com>
2021-04-18 10:12:44 +0200

    removed commented out code from demo

--- demo/app/main.ts
+++ demo/app/main.ts
@@ -1,10 +1,7 @@
 import 'core-js/stable';
 import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
-// import LogRocket from 'logrocket';
 import {bootstrap} from 'web-component-builder';
 
 import {MainModule} from './main.module';
-// init monitoring for app performance
-// LogRocket.init('jr8nqw/web-component-builder');
 // bootstrap module
 bootstrap(MainModule);
